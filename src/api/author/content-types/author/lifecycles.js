e939b384b5422679df24ab1c5775cda3796376dc
Anuradha Ingavale <anuradha@suretaas.com>
2026-06-05 15:47:47 +0530

    Add slug generation to content types

---
+++ src/api/author/content-types/author/lifecycles.js
@@ -0,0 +1,25 @@
+module.exports = {
+    beforeCreate(event) {
+      const { data } = event.params;
+  
+      if (data.title) {
+        data.slug = data.title
+          .toLowerCase()
+          .trim()
+          .replace(/[^\w\s-]/g, '')
+          .replace(/\s+/g, '-');
+      }
+    },
+  
+    beforeUpdate(event) {
+      const { data } = event.params;
+  
+      if (data.title) {
+        data.slug = data.title
+          .toLowerCase()
+          .trim()
+          .replace(/[^\w\s-]/g, '')
+          .replace(/\s+/g, '-');
+      }
+    },
+  };
